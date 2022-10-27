12ca08f82b266c85788633741237b750ca4fcf4b
JangWoojun <woojun0107@naver.com>
2022-10-27 18:37:32 +0900

    2022/10/27 view binding 추가

--- app/src/main/java/com/example/smartcalculator/MainActivity.kt
+++ app/src/main/java/com/example/smartcalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.smartcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
