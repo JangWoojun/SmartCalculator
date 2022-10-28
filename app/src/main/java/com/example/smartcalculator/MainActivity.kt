97bd022a06f63c97e3bc682593340752d16fb484
JangWoojun <woojun0107@naver.com>
2022-10-29 01:16:04 +0900

    2022/10/29 숫자 키패드 클릭 시 계산 칸에 입력한 숫자 보이기 추가

--- app/src/main/java/com/example/smartcalculator/MainActivity.kt
+++ app/src/main/java/com/example/smartcalculator/MainActivity.kt
@@ -12,7 +12,77 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//      키패드
 
+        binding.btn0.setOnClickListener {
+            binding.textView.append("0")
+        }
+        binding.btn1.setOnClickListener {
+            binding.textView.append("1")
+        }
+        binding.btn2.setOnClickListener {
+            binding.textView.append("2")
+        }
+        binding.btn3.setOnClickListener {
+            binding.textView.append("3")
+        }
+        binding.btn4.setOnClickListener {
+            binding.textView.append("4")
+        }
+        binding.btn5.setOnClickListener {
+            binding.textView.append("5")
+        }
+        binding.btn6.setOnClickListener {
+            binding.textView.append("6")
+        }
+        binding.btn7.setOnClickListener {
+            binding.textView.append("7")
+        }
+        binding.btn8.setOnClickListener {
+            binding.textView.append("8")
+        }
+        binding.btn9.setOnClickListener {
+            binding.textView.append("9")
+        }
+
+//      보조키
+
+        binding.btnX.setOnClickListener {
+
+        }
+        binding.btnChange.setOnClickListener {
+
+        }
+        binding.btnM.setOnClickListener {
+
+        }
+        binding.btnFahrenheit.setOnClickListener {
+
+        }
+        binding.btnAcreage.setOnClickListener {
+
+        }
+        binding.btnInch.setOnClickListener {
+
+        }
+
+//      연산자
+
+        binding.btnPlus.setOnClickListener {
+
+        }
+        binding.btnMinus.setOnClickListener {
+
+        }
+        binding.btnObelus.setOnClickListener {
+
+        }
+        binding.btnTimes.setOnClickListener {
+
+        }
+        binding.btnEqual.setOnClickListener {
+
+        }
 
     }
 }
